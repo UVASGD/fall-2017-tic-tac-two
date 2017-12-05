12181b8e5745d7dde761eae02c7e6b1ee1ec81ac
jpat64 <jrp3tu@virginia.edu>
2017-12-05 16:05:35 -0500

    Win conditions for modifiable TTT, also game reset

--- TTT/src/ModifiableTTT.java
+++ TTT/src/ModifiableTTT.java
@@ -20,6 +20,8 @@ public class ModifiableTTT extends JFrame
 	String activePlayer;
 	//playerIndex is always either -1 or 1
 	int playerIndex;
+	JLabel player;
+	JLabel gameInfo;
 	
 	int[][] values;
 	JButton[][] buttons;
@@ -28,6 +30,7 @@ public class ModifiableTTT extends JFrame
 	
 	int winner;
 	boolean winning;
+	int longest;
 	
 	public ModifiableTTT()
 	{
@@ -60,6 +63,9 @@ public class ModifiableTTT extends JFrame
 		int horizspace = rightedge - leftedge;
 		int vertspace = bottomedge - topedge;
 		
+		int rightbuffer = WIDTH - rightedge;
+		int vertsubsection = HEIGHT/10;
+		
 		int buttonhorizspace = horizspace / dimension;
 		System.out.println(leftedge + " " + horizspace + " " + buttonhorizspace);
 		int buttonvertspace = vertspace / dimension;
@@ -83,16 +89,30 @@ public class ModifiableTTT extends JFrame
 			for (int j = 0; j < dimension; j++)
 			{
 				buttons[i][j] = new JButton();
-				dropButton(locations[i*dimension + j], sizes[i*dimension + j], new int[]{i, j}, true, true);
+				dropButton(locations[i*dimension + j], sizes[i*dimension + j], new int[]{i, j}, true);
 			}
 		}
+		
+		//put the rest of the UI on the board
+		player = new JLabel("Active player: " + activePlayer);
+		drop(new int[]{rightedge, topedge}, new int[]{(int)(rightbuffer * 0.75), (int)(vertsubsection * 0.75)}, true, player);
+		gameInfo = new JLabel();
+		updateGameInfo(0);
+		drop(new int[]{rightedge, topedge + vertsubsection}, new int[]{(int)(rightbuffer * 0.75), (int)(vertsubsection * 5 + vertsubsection * 0.75)}, true, gameInfo);
 		
 		add(new JLabel());
 		setVisible(true);
 	}
 	
+	public void drop(int[] pos, int[] size, boolean visibility, JComponent thing)
+	{
+		thing.setSize(size[0], size[1]);
+		thing.setLocation(pos[0], pos[1]);
+		thing.setVisible(visibility);
+		add(thing);
+	}
 	
-	public void dropButton(int[] pos, int[] size, int[] coords, boolean visibility, boolean isButton)
+	public void dropButton(int[] pos, int[] size, int[] coords, boolean visibility)
 	{
 		JButton thing = buttons[coords[0]][coords[1]];
 		
@@ -126,7 +146,8 @@ public class ModifiableTTT extends JFrame
 			//activated.setText(activePlayer);
 			values[coords[0]][coords[1]] = playerIndex;
 
-			checkWin(0,0,0);
+			checkWin(coords[0], coords[1]);
+			
 			if (winning)
 			{
 				JOptionPane.showMessageDialog(null, activePlayer + " has won!");
@@ -143,75 +164,245 @@ public class ModifiableTTT extends JFrame
 			{
 				activePlayer = "X";
 			}	
+			player.setText("Active Player: " + activePlayer);
 		}
 	}
 	
-	public void checkWin(int width, int height, int streak)
+	
+	/* EDGE CASES:
+	 * T: only check 2+6, 3, 4, and 5
+	 * TR: only check 4, 5, and 6
+	 * R: only check 0+4, 5, 6, and 7
+	 * BR: only check 0, 6, and 7
+	 * B: only check 0, 1, 2+6, and 7
+	 * BL: only check 0, 1, and 2
+	 * L: only check 0+4, 1, 2, and 3
+	 * TL: only check 2, 3, and 4
+	 * 
+	 * Otherwise, check 0+4, 1+5, 2+6, and 3+7
+	 */
+	public void checkWin(int width, int height)
 	{
-		boolean w = false;
-		boolean h = false;
-		if (width + 1 < values.length)
+		//make boolean variables signifying our types of edge cases
+		boolean t = false, b = false, r = false, l = false;
+		if (width == 0)
+			l = true;
+		if (width == dimension - 1)
+			r = true;
+		if (height == 0)
+			t = true;
+		if (height == dimension - 1)
+			b = true;
+		
+		int streak = 0;
+		if (l || r || t || b)
 		{
-			w = true;
-		}
-		if (height + 1 < values[width].length)
-		{
-			h = true;
-		}
-		if (h && w)
-		{
-			if (values[width][height] == values[width + 1][height + 1] && values[width][height] != 0)
+			if (l && t)
 			{
-				streak++;
+				System.out.println("Doing the top left comparison at [" + width + "][" + height + "]");
+				streak = Integer.max(Integer.max(checkWin(width, height, 0, 2), checkWin(width, height, 0, 3)), checkWin(width, height, 0, 4));
+			}
+			else if (l && b)
+			{
+				System.out.println("Doing the bottom left comparison at [" + width + "][" + height + "]");
+				streak = Integer.max(Integer.max(checkWin(width, height, 0, 0), checkWin(width, height, 0, 1)), checkWin(width, height, 0, 2));
+			}
+			else if (r && t)
+			{
+
+				System.out.println("Doing the top right comparison at [" + width + "][" + height + "]");
+				streak = Integer.max(Integer.max(checkWin(width, height, 0, 4), checkWin(width, height, 0, 5)), checkWin(width, height, 0, 6));
+			}
+			else if (r && b)
+			{
+
+				System.out.println("Doing the bottom right comparison at [" + width + "][" + height + "]");
+				streak = Integer.max(Integer.max(checkWin(width, height, 0, 6), checkWin(width, height, 0, 7)), checkWin(width, height, 0, 8));
+			}
+			else if (t) //make sure we hit the easy-to-satisfy cases last
+			{
+
+				System.out.println("Doing the top comparison at [" + width + "][" + height + "]");
+				streak = Integer.max(Integer.max(checkWin(width, height, 0, 3), checkWin(width, height, 0, 5)), Integer.max(checkWin(width, height, 0, 4), checkWin(width, height, 0, 2) + checkWin(width, height, 0, 6)));
+			}
+			else if (b)
+			{
+
+				System.out.println("Doing the bottom comparison at [" + width + "][" + height + "]");
+				streak = Integer.max(Integer.max(checkWin(width, height, 0, 1), checkWin(width, height, 0, 7)), Integer.max(checkWin(width, height, 0, 0), checkWin(width, height, 0, 2) + checkWin(width, height, 0, 6)));
+			}
+			else if (l)
+			{
+
+				System.out.println("Doing the left comparison at [" + width + "][" + height + "]");
+				streak = Integer.max(Integer.max(checkWin(width, height, 0, 3), checkWin(width, height, 0, 1)), Integer.max(checkWin(width, height, 0, 2), checkWin(width, height, 0, 0) + checkWin(width, height, 0, 4)));
+			}
+			else if (r)
+			{
+
+				System.out.println("Doing the right comparison at [" + width + "][" + height + "]");
+				streak = Integer.max(Integer.max(checkWin(width, height, 0, 5), checkWin(width, height, 0, 7)), Integer.max(checkWin(width, height, 0, 6), checkWin(width, height, 0, 0) + checkWin(width, height, 0, 4)));
 			}
 			else
 			{
-				streak = 0;
+
+				System.out.println("Shouldn't be here at [" + width + "][" + height + "]");
 			}
-			if (streak == winner)
-			{
-				winning = true;
-			}
-			checkWin(width + 1, height + 1, streak);
 		}
-		if (h)
+		else
 		{
-			if (values[width][height] == values[width][height+1] && values[width][height] != 0)
-			{
-				streak++;
-			}
-			else
-			{
-				streak = 0;
-			}
-			if (streak == winner)
-			{
-				winning = true;
-			}
-			checkWin(width, height + 1, streak);
-		}
-		if (w)
-		{
-			if (values[width][height] == values[width+1][height] && values[width][height] != 0)
-			{
-				streak++;
-			}
-			else
-			{
-				streak = 0;
-			}
-			if (streak == winner)
-			{
-				winning = true;
-			}
-			checkWin(width + 1, height, streak);
+			System.out.println("Doing the full comparison at [" + width + "][" + height + "]");
+			streak = Integer.max(Integer.max(checkWin(width, height, 0, 0) + checkWin(width, height, 0, 4), checkWin(width, height, 0, 1) + checkWin(width, height, 0, 5)), Integer.max(checkWin(width, height, 0, 2) + checkWin(width, height, 0, 6), checkWin(width, height, 0, 3) + checkWin(width, height, 0, 7)));
 		}
 		
+		if (streak + 1 >= winner)
+		{
+			winning = true;
+		}
+	}
+	
+	/* DIRECTION INDEX:
+	 * 0 = T
+	 * 1 = TR
+	 * 2 = R
+	 * 3 = BR
+	 * 4 = B
+	 * 5 = BL
+	 * 6 = L
+	 * 7 = TL
+	 */
+	public int checkWin(int width, int height, int streak, int direction)
+	{
+		switch(direction)
+		{
+		case 0:
+			System.out.println("Checking ^ at [" + width + "][" + height + "]");
+			if (height != 0)
+			{
+				if (values[width][height] == values[width][height - 1])
+				{
+					streak ++;
+					System.out.println("Still going. " + streak);
+					return checkWin(width, height - 1, streak, direction);
+				}
+			}
+			System.out.println("Stopping. " + streak);
+			return streak;
+		case 1:
+			System.out.println("Checking /^ at [" + width + "][" + height + "]");
+			if (height != 0 && width != dimension - 1)
+			{
+				if (values[width][height] == values[width + 1][height - 1])
+				{
+					streak ++;
+					System.out.println("Still going. " + streak);
+					return checkWin(width + 1, height - 1, streak, direction);
+				}
+			}
+			System.out.println("Stopping. " + streak);
+			return streak;
+		case 2:
+			System.out.println("Checking > at [" + width + "][" + height + "]");
+			if (width != dimension - 1)
+			{
+				if (values[width][height] == values[width + 1][height])
+				{
+					streak ++;
+					System.out.println("Still going. " + streak);
+					return checkWin(width + 1, height, streak, direction);
+				}
+			}
+			System.out.println("Stopping. " + streak);
+			return streak;
+		case 3:
+			System.out.println("Checking \\v at [" + width + "][" + height + "]");
+			if (height != dimension - 1 && width != dimension - 1)
+			{
+				if (values[width][height] == values[width + 1][height + 1])
+				{
+					streak ++;
+					System.out.println("Still going. " + streak);
+					return checkWin(width + 1, height + 1, streak, direction);
+				}
+			}
+			System.out.println("Stopping. " + streak);
+			return streak;
+		case 4:
+			System.out.println("Checking v at [" + width + "][" + height + "]");
+			if (height != dimension - 1)
+			{
+				if (values[width][height] == values[width][height + 1])
+				{
+					streak ++;
+					System.out.println("Still going. " + streak);
+					return checkWin(width, height + 1, streak, direction);
+				}
+			}
+			System.out.println("Stopping. " + streak);
+			return streak;
+		case 5:
+			System.out.println("Checking v/ at [" + width + "][" + height + "]");
+			if (height != dimension - 1 && width != 0)
+			{
+				if (values[width][height] == values[width - 1][height + 1])
+				{
+					streak ++;
+					System.out.println("Still going. " + streak);
+					return checkWin(width - 1, height + 1, streak, direction);
+				}
+			}
+			System.out.println("Stopping. " + streak);
+			return streak;
+		case 6:
+			System.out.println("Checking < at [" + width + "][" + height + "]");
+			if (width != 0)
+			{
+				if (values[width][height] == values[width - 1][height])
+				{
+					streak ++;
+					System.out.println("Still going. " + streak);
+					return checkWin(width - 1, height, streak, direction);
+				}
+			}
+			System.out.println("Stopping. " + streak);
+			return streak;
+		case 7:
+			System.out.println("Checking ^\\ at [" + width + "][" + height + "]");
+			if (height != 0 && width != 0)
+			{
+				if (values[width][height] == values[width - 1][height - 1])
+				{
+					streak ++;
+					System.out.println("Still going. " + streak);
+					return checkWin(width - 1, height - 1, streak, direction);
+				}
+			}
+			System.out.println("Stopping. " + streak);
+			return streak;
+		}
+		return 0;
 	}
 	
 	public void reset()
 	{
 		System.out.println("Resetting...");
+		for (int i = 0; i < values.length; i++)
+		{
+			for (int j = 0; j < values[i].length; j++)
+			{
+				values[i][j] = 0;
+				buttons[i][j].setBackground(Color.WHITE);
+			}
+		}
+	}
+	
+	public void updateGameInfo(int longest)
+	{
+		String info = "";
+		info += "Grid size: " + dimension + "x" + dimension + "<br/>";
+		info += "Score to win: " + winner + " in a row.<br/>";
+		info += "Longest current line: " + longest + " spaces.";
+		gameInfo.setText("<html>" + info + "<html>");
 	}
 }
 
